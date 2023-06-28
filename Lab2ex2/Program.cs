ac255b006d17d5960fdf6a2966eec0afd1e38c06
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-06-28 15:11:40 +0300

    Add project files.

---
+++ Lab2ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab2ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex 2: Scrieti un program care va afisa factorialul unui numar n, n fiind citit
+            de la tastatura.*/
+
+            Console.WriteLine("FACTORIAL");
+            Console.WriteLine("Introduceti un numar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            for(int i = n; i >= 1; i--)
+            {
+                factorial *= i; //inmulteste cu fiecare numar
+            }
+            Console.WriteLine("Factorialul din " + n + " Este " + factorial);
+        }
+    }
+}
